08a151390c99e450fa03d16fd6f50d5727210549
Rodrigo Nunes <brksnunes@protonmail.com>
2022-11-17 23:53:22 +0000

    feat: :card_file_box: Added "products" Model main funcs

---
+++ src/models/products.model.js
@@ -0,0 +1,20 @@
+const connection = require('./database/connection');
+
+const getAll = async () => {
+  const [result] = await connection.execute(
+    'SELECT * FROM StoreManager.products ORDER BY id',
+  );
+  return result;
+};
+
+const getById = async (id) => { 
+  const [result] = await connection.execute(
+    'SELECT * FROM StoreManager.products WHERE id = ? ORDER BY id', [id],
+  );
+  return result;
+};
+
+module.exports = {
+  getAll,
+  getById,
+};
