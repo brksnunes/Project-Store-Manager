571235d1475ea3233151286d7824b514e91b2b62
Rodrigo Nunes <brksnunes@protonmail.com>
2022-11-18 15:17:35 +0000

    feat: :sparkles: Added getById function on sales controller

--- src/controllers/sales.controller.js
+++ src/controllers/sales.controller.js
@@ -5,6 +5,14 @@ const getSales = async (_req, res) => {
   res.status(200).json(message);
 };
 
+const getSalesById = async (req, res) => { 
+  const { id } = req.params;
+  const { type, message } = await salesService.getById(id);
+  if (type) return res.status(type).json({ message });
+  res.status(200).json(...message);
+};
+
 module.exports = {
   getSales,
+  getSalesById,
 };
