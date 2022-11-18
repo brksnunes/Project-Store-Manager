53f977547821ede21ccf6464e7c767c3798f9c44
Rodrigo Nunes <brksnunes@protonmail.com>
2022-11-18 15:13:52 +0000

    feat: :sparkles: Added getByID function on sales model

--- src/models/sales.model.js
+++ src/models/sales.model.js
@@ -11,10 +11,26 @@ FROM
     StoreManager.sales_products AS sales_prod
         INNER JOIN
     StoreManager.sales AS sales ON sales.id = sales_prod.sale_id
-ORDER BY sales_prod.sale_id , sales_prod.product_id;`);
+ORDER BY sales_prod.sale_id , sales_prod.product_id`);
+  return result;
+};
+
+const getById = async (id) => { 
+  const [result] = await connection.execute(`
+  SELECT 
+    sales_prod.product_id AS productId,
+    sales_prod.quantity,
+    sales.date
+FROM
+    StoreManager.sales_products AS sales_prod
+        INNER JOIN
+    StoreManager.sales AS sales ON sales.id = sales_prod.sale_id
+        WHERE sales.id = ?
+ORDER BY sales_prod.sale_id , sales_prod.product_id`, [id]);
   return result;
 };
 
 module.exports = {
   getAll,
+  getById,
 };
